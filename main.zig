8724199a0194a6cfb9aedf6e908b2fef1a2fe9ac
Sören Tempel <soeren+git@soeren-tempel.net>
2020-04-10 18:55:33 +0200

    Use an unsigned integer type for addresses

--- main.zig
+++ main.zig
@@ -1,15 +1,15 @@
 // UART control base addresses.
-const UART0_CTRL_ADDR: i32 = 0x10013000;
-const UART1_CTRL_ADDR: i32 = 0x10023000;
+const UART0_CTRL_ADDR: u32 = 0x10013000;
+const UART1_CTRL_ADDR: u32 = 0x10023000;
 
 // Offsets for memory mapped UART control registers.
-const UART_REG_TXFIFO : i32 = 0x00;
-const UART_REG_RXFIFO : i32 = 0x04;
-const UART_REG_TXCTRL : i32 = 0x08;
-const UART_REG_RXCTRL : i32 = 0x0c;
-const UART_REG_IE     : i32 = 0x10;
-const UART_REG_IP     : i32 = 0x14;
-const UART_REG_DIV    : i32 = 0x18;
+const UART_REG_TXFIFO : u32 = 0x00;
+const UART_REG_RXFIFO : u32 = 0x04;
+const UART_REG_TXCTRL : u32 = 0x08;
+const UART_REG_RXCTRL : u32 = 0x0c;
+const UART_REG_IE     : u32 = 0x10;
+const UART_REG_IP     : u32 = 0x14;
+const UART_REG_DIV    : u32 = 0x18;
 
 export fn myinit() void {
   asm volatile ("addi t0, t0, 23");
