9623e84332f853753a821a4161abd2b267db2cad
Sören Tempel <soeren+git@soeren-tempel.net>
2020-04-10 20:23:32 +0200

    build.sh: Reformat for clarity

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-zig build-exe main.zig -target riscv32-freestanding-none --linker-script fe310_g000.ld --c-source start.S
+zig build-exe main.zig \
+	-target riscv32-freestanding-none \
+	--linker-script fe310_g000.ld \
+	--c-source start.S
