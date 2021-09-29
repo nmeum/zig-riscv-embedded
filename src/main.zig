bb7ac31fccb6b66c471aa5024285b3bc6f974170
Sören Tempel <soeren+git@soeren-tempel.net>
2021-09-29 18:23:57 +0200

    main: configure slipmux again

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
 const console = @import("console.zig");
+const slipmux = @import("slipmux.zig");
+const periph = @import("periph.zig");
 
 pub fn main() !void {
-    console.print("Hello, World!\n", .{});
+    const smux = slipmux.SlipMux.init(periph.uart1, periph.plic0);
+    console.print("Waiting for incoming CoAP packets over UART1...\n", .{});
 }
